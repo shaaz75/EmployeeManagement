f9e90d1dfc7042eca6e03cd07fe4e7e77ced27eb
sajid <sajidahmed.77@gmail.com>
2020-01-06 21:08:23 +0300

    Edit identity user

--- EmployeeManagement/Controllers/AdminController.cs
+++ EmployeeManagement/Controllers/AdminController.cs
@@ -88,56 +88,59 @@ namespace EmployeeManagement.Controllers
             }
         }
 
-        //[HttpGet]
-        //public async Task<IActionResult> EditUser(string id)
-        //{
-        //    var user = await roleManager.FindByIdAsync(id);
-        //    if (user == null)
-        //    {
-        //        ViewBag.ErrorMessage = $"User with Id={id} cannot be found";
-        //        return View("NotFound");
-        //    }
-        //    var model = new EditRoleViewModel()
-        //    {
-        //        Id = role.Id,
-        //        RoleName = role.Name
-        //    };
+        [HttpGet]
+        public async Task<IActionResult> EditUser(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id={id} cannot be found";
+                return View("NotFound");
+            }
 
-        //    foreach (var user in this.userManager.Users)
-        //    {
-        //        if (await this.userManager.IsInRoleAsync(user, role.Name))
-        //        {
-        //            model.Users.Add(user.UserName);
-        //        }
-        //    }
-        //    return View(model);
-        //}
+            var userClaims = await userManager.GetClaimsAsync(user);
+            var userRoles = await userManager.GetRolesAsync(user);
+            var model = new EditUserViewModel()
+            {
+                City = user.City,
+                Email = user.Email,
+                Id = user.Id,
+                UserName = user.UserName,
+                Claims = userClaims.Select(s => s.Value).ToList(),
+                Roles=userRoles.ToList()
+            };
 
-        //[HttpPost]
-        //public async Task<IActionResult> EditRole(EditRoleViewModel model)
-        //{
-        //    var role = await roleManager.FindByIdAsync(model.Id);
-        //    if (role == null)
-        //    {
-        //        ViewBag.ErrorMessage = $"Role with Id={model.Id} cannot be found";
-        //        return View("NotFound");
-        //    }
-        //    else
-        //    {
-        //        role.Name = model.RoleName;
-        //        var result = await roleManager.UpdateAsync(role);
-        //        if (result.Succeeded)
-        //        {
-        //            return RedirectToAction("ListRoles", "Admin");
-        //        }
-        //        foreach (IdentityError error in result.Errors)
-        //        {
-        //            ModelState.AddModelError("", error.Description);
-        //        }
+            return View(model);
+        }
 
-        //        return View(model);
-        //    }
-        //}
+        [HttpPost]
+        public async Task<IActionResult> EditUser(EditUserViewModel model)
+        {
+            var user = await userManager.FindByIdAsync(model.Id);
+            if (user == null)
+            {
+                ViewBag.ErrorMessage = $"User with Id={model.Id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                user.Email = model.Email;
+                user.UserName = model.UserName;
+                user.City = model.City;
+
+                var result = await userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListUsers", "Admin");
+                }
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+                return View(model);
+            }
+        }
 
         [HttpGet]
         public async Task<IActionResult> EditUsersInRole(string roleId)
